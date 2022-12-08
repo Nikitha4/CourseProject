edad7363f281b0fe36c926f9701910fa1c7d78ab
Nikitha4 <55000161+Nikitha4@users.noreply.github.com>
2022-12-08 13:43:31 -0600

    Add files via upload

--- project.py
+++ project.py
@@ -3,11 +3,6 @@ from collections import defaultdict
 from collections import Counter
 
 
-
-#print(data)
-
-
-
 def rankings(searchengineresult):
 
     names = []
@@ -15,14 +10,7 @@ def rankings(searchengineresult):
     for x in searchengineresult:
         names.append(x[0])
 
-    
     counts = Counter(names)
-
-    #print(counts)
-
-    #orderedres = []
-
-    
 
     return dict(sorted(counts.items(), key=lambda item: item[1]))
 
@@ -32,17 +20,7 @@ def rankings(searchengineresult):
 
 def searchengine(wordstomes, query) :
 
-    #messages = wordstomes[query]
-
     querywords = query.split()
-
-    #querymessages = []
-
-    #for x in querywords:
-    #    querymessages.append(wordstomes[x])
-
-    
-    #for i in range(len(querymessages)):
 
     newq = querywords[0]
 
@@ -53,59 +31,45 @@ def searchengine(wordstomes, query) :
         if query in x[1]:
             finalmessages.append(x)
 
-
-
-
     return finalmessages
 
 
 
 def main() :
-    with open("data.json", "r") as read_file:
+
+    jsonfilename = input("Enter JSON File Name: ")
+
+    with open(jsonfilename, "r") as read_file:
         data = json.load(read_file)
 
     participants = []
     for i in range(len(data["participants"])):
         participants.append(data["participants"][i]["name"])
 
-    #print(participants)
-    #print(data["participants"][0]["name"])
-
     num_participants = len(participants)
 
-    #message[] = (sendername, content)
     messages = []
     for i in range(len(data["messages"])):
         messages.append((data["messages"][i]["sender_name"], data["messages"][i]["content"]))
-
-    #print(messages)
 
     words_to_messages = defaultdict(list)
 
     for i in range((len(messages))):
         mescontent = messages[i][1]
         contentwords = mescontent.split()
-        #print(contentwords)
         for j in range(len(contentwords)):
-            
             words_to_messages[contentwords[j]].append(messages[i])
-            #print( words_to_messages[contentwords[j]])
-    #print(words_to_messages)
+            
+    q = input("Query: ")
 
-    m = searchengine(words_to_messages, "im ready")
-    #print(m)
-    #for i in range(len(m)):
-        #print("#: " + str(i) + ", Name: " + m[i][0] + ", Content: " + m[i][1])   
+    m = searchengine(words_to_messages, q)
 
-
-    print(m)
+    for i in range(len(m)):
+        print("Result #: " + str(i + 1) + ", Name: " + m[i][0] + ", Content: " + m[i][1])   
 
     f = rankings(m)
 
-    #print(f)
-
-
-
+    print(f)
 
 
 main()
